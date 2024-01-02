2ee7b2eeb1f7868361a481cf050d595e4cb12174
shaneDawsonKitten <124752924+shaneDawsonKitten@users.noreply.github.com>
2024-01-02 09:40:00 +0800

    Delete preview.php

--- preview.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['tempFile']) && file_exists($_GET['tempFile'])) {
-    $tempFile = $_GET['tempFile'];
-    header("Content-type: image/png");
-    readfile($tempFile);
-} else {
-    echo "Invalid request.";
-}
-?>
